94c60e619f64b9e53c60c164b49a7eef193f29f7
anshumanmishhra1 <anshumanmishhra1@gmail.com>
2024-10-02 12:03:17 +0530

    last

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "../components/UI/HeroSection";
+import About from "./About";
+
+const Home = () => {
+  return <>
+    <HeroSection />
+    <About />
+  </>
+};
+
+export default Home;
